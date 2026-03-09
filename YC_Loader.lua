c888af47bc2cb69873dc5712becbe132e72257d6
SevereYC <85minecraft666666@gmail.com>
2026-03-10 04:43:08 +0800

    Update YC_Loader.lua

--- YC_Loader.lua
+++ YC_Loader.lua
@@ -1,10 +1,26 @@
+print("Script Loader Started")
+
 local Scripts = {
     [847722000] = "https://raw.githubusercontent.com/SevereYC/Severe_git/refs/heads/main/The_Rake.lua",
     [2165551367] = "https://raw.githubusercontent.com/SevereYC/Severe_git/refs/heads/main/Granny.lua"
 }
 
-local url = Scripts[game.PlaceId]
+local GameId = game.GameId
+print("Current GameId:", GameId)
+
+local url = Scripts[GameId]
 
 if url then
-    loadstring(game:HttpGet(url))()
+    print("found game")
+    print("Loading script...")
+
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+
+    if not success then
+        warn("Script execution failed:", err)
+    end
+else
+    warn("No script configured for this GameId.")
 end
