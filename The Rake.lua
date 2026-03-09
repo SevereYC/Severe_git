1b3541777544e235c5ec2ad80b35782be2a6a7d6
SevereYC <85minecraft666666@gmail.com>
2026-03-10 04:26:45 +0800

    Add files via upload

---
+++ The Rake.lua
@@ -0,0 +1,5 @@
+-- works in most resolutions [1440p and under]
+-- no need to enable unsafe LuaU execution
+-- restart luavm if anything breaks
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kznkez/the-bake-remastered/refs/heads/main/rake.lua"))()
