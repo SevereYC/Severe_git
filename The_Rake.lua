7807ecfe52a5747d5353070d40478352ce4a2f51
SevereYC <85minecraft666666@gmail.com>
2026-03-10 04:48:34 +0800

    Update The_Rake.lua

--- The_Rake.lua
+++ The_Rake.lua
@@ -1,5 +1,1 @@
--- works in most resolutions [1440p and under]
--- no need to enable unsafe LuaU execution
--- restart luavm if anything breaks
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kznkez/the-bake-remastered/refs/heads/main/rake.lua"))()
