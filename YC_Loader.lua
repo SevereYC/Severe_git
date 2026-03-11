d698d3ee0565723526e50cbe49ff033b4f3387e6
SevereYC <85minecraft666666@gmail.com>
2026-03-11 09:35:14 +0800

    Update notification duration in YC_Loader.lua

--- YC_Loader.lua
+++ YC_Loader.lua
@@ -1,5 +1,5 @@
 print("Script Loader Started")
-notify("Loading...","YCLoader",4)
+notify("Loading...","YCLoader",5)
 
 local Scripts = {
     [847722000] = "https://raw.githubusercontent.com/SevereYC/Severe_git/refs/heads/main/The_Rake.lua",
@@ -25,5 +25,5 @@ if url then
     end
 else
     warn("No script configured for this GameId.")
-    notify("Unsupported games","YCLoader",2)
+    notify("Unsupported games","YCLoader",5)
 end
